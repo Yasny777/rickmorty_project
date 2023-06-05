239836d67d4714f3e97486de764b43c56bdb04dc
Yasny <jjasnowski01@gmail.com>
2023-06-05 19:31:14 +0200

    Initalize Project
    Create workspace and folder structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="w-full h-[70px] bg-myLime text-3xl font-['Lato-Bold'] flex justify-center items-center">
+      <h1 className="uppercase">Lorem Ipsum</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
