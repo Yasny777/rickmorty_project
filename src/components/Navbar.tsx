3ac1c7153021f63ade9f98827f8a6338d697b1e3
Yasny <jjasnowski01@gmail.com>
2023-06-12 21:41:39 +0200

    Added styles for mobile and desktop version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = (): JSX.Element => {
   return (
-    <nav className="w-full h-[70px] bg-myLime text-3xl font-['Lato-Bold'] flex justify-center items-center">
+    <nav className="w-full h-[70px] bg-myLime text-3xl font-['Lato-Bold'] flex justify-center items-center lg:h-24 lg:justify-start lg:pl-16 lg:pb-1 lg:text-[35px] after:content-['']">
       <h1 className="uppercase">Lorem Ipsum</h1>
     </nav>
   );
